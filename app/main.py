bbd00c37841d1a5e644aceea3e1da8b7631aa9f0
a.parviz <a.m.parviz@gmail.com>
2026-08-26 13:11:23 +0330

    creating project

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import Any
+
+import psutil
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/health")
+async def salamat_check() -> dict[str, Any]:
+    cpu_perc = psutil.cpu_percent()
+    memory = psutil.virtual_memory()
+    uptime = datetime.now() - datetime.fromtimestamp(psutil.boot_time())
+
+    return {
+        "status": "healthy",
+        "system":{
+            "CPU Percent" : cpu_perc,
+            "Memory Percent": memory.percent,
+            "UpTime": uptime.total_seconds()
+        }
+    }
